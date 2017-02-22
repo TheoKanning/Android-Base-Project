3b31fd809170c47090489c3e7dc0fcccb2dbd773
Theo Kanning <theokanning@gmail.com>
2017-02-21 21:43:20 -0600

    Added leak canary and timber

--- app/src/main/java/theo/base/AppInitializer.java
+++ app/src/main/java/theo/base/AppInitializer.java
@@ -1,12 +1,15 @@
 package theo.base;
 
 
+import com.squareup.leakcanary.LeakCanary;
+
 import net.danlew.android.joda.JodaTimeAndroid;
 
 public abstract class AppInitializer {
 
     void init(BaseApplication app){
         JodaTimeAndroid.init(app);
+        LeakCanary.install(app);
         buildTypeDependentInitialization(app);
     }
 
--- app/src/main/java/theo/base/BaseApplication.java
+++ app/src/main/java/theo/base/BaseApplication.java
@@ -3,6 +3,8 @@ package theo.base;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import javax.inject.Inject;
 
 import theo.base.dagger.BaseComponent;
@@ -17,6 +19,11 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
 
         buildComponentAndInject();
 

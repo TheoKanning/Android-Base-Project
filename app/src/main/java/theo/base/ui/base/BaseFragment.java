0df1e1d22048e293ecc50353880c342fce873465
Theo Kanning <theokanning@gmail.com>
2017-12-09 15:55:04 -0600

    Update gradle plugin

--- app/src/main/java/theo/base/ui/base/BaseFragment.java
+++ app/src/main/java/theo/base/ui/base/BaseFragment.java
@@ -1,11 +1,8 @@
 package theo.base.ui.base;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 import butterknife.Unbinder;
-import icepick.Icepick;
 
 import static dagger.internal.Preconditions.checkNotNull;
 
@@ -13,18 +10,6 @@ import static dagger.internal.Preconditions.checkNotNull;
 public abstract class BaseFragment extends Fragment implements BaseActivity.OnBackPressedListener{
 
     private Unbinder unbinder = Unbinder.EMPTY;
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        Icepick.restoreInstanceState(this, savedInstanceState);
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        Icepick.saveInstanceState(this, outState);
-        super.onSaveInstanceState(outState);
-    }
 
     @Override
     public void onDestroyView() {
